37dfea14ecb1e05b8bb971cbc509d7cb470d9eec
Amrhitzz <amrhitzz@gmail.com>
2020-10-21 10:00:56 +0545

    amrhit first day one task

---
+++ AmrhitPradhan-Day1-Task/AttendeeList.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const AttendeeList = () => {
+    const [name, setName] =useState("");
+    const [number, setNumber] = useState("");
+    let Attendee= [];
+    const [msg, setMsg] =useState(Attendee);
+
+    const nameEvent =(event)=>{
+        setName(event);
+    }
+
+    const numberEvent=(event)=>{
+        setNumber(event);
+    }
+
+    const btn = () => {
+        console.log(Attendee);
+        setMsg([...msg, {
+            Name: name,
+            Number: number
+        }])
+      setName('');
+      setNumber('');
+    };
+
+    return (
+        <>
+                <input type="text" placeholder="Enter Name" onChange={(e)=>{nameEvent(e.target.value)}} value={name}/> <br/> <br/>
+                <input type="number" placeholder="Enter Number" onChange={(e)=>{numberEvent(e.target.value)}} value={number}/> <br/> <br/>
+                <button onClick={btn}> Submit </button>
+
+                <ol>
+                { msg.map((value,index)=>{
+                  return  <li key={index}> Name: {value.Name} <br/> Number: {value.Number} </li>
+                }) }
+                </ol>
+        </>
+    );
+}
+
+export default AttendeeList;
---
+++ AmrhitPradhan-Day1-Task/IncreDecre.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+const IncreDecre = () => {
+
+    let initial = 0, temp = 2;
+
+    const [value, setValue] = useState(initial);
+    const [msg, setMsg] = useState("Increase or Decrease");
+
+    const increBtn = () => {
+        setMsg("");
+        setValue(prevValue=> prevValue + temp);
+    }
+
+    const decreBtn = () => {
+        (value!==0) ? setValue(prevValue=> prevValue - temp) : setMsg(`Value can't be less than ${initial}`);
+    }
+    const reset =() =>{
+        setValue(initial);
+    }
+
+    return (
+        <>
+        <div className="firstTask">
+            <h4> Increment and Decrement </h4>
+            <span> {value} </span> <br />
+            <button onClick={increBtn}>Increment</button>
+            <button onClick={decreBtn}>Decrement</button>
+            <button onClick={reset}>Reset</button>
+            <p> {msg} </p>
+        </div>
+        </>
+    );
+}
+
+export default IncreDecre;
