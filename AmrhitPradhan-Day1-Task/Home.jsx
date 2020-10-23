66f6b6008ae76562f3eb0a761f81823ec297858e
Amrhitzz <amrhitzz@gmail.com>
2020-10-23 14:40:38 +0545

    edited index.css

--- AmrhitPradhan-Day1-Task/Home.jsx
+++ AmrhitPradhan-Day1-Task/Home.jsx
@@ -6,8 +6,17 @@ const Home =( {logout})=>{
   }
   return (
     <>
+    <div className="homesection">
+
+    
     <h3>MISSION PASSED!!!!!</h3>
+    <h3>Welcome To Home Section</h3>
+   <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel fuga consequatur voluptatum ratione. Cumque libero voluptas esse, at animi repellendus, quibusdam facere veniam dolor tempore, fugiat perferendis eius voluptatem repellat.
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel fuga consequatur voluptatum ratione. Cumque libero voluptas esse, at animi repellendus, quibusdam facere veniam dolor tempore, fugiat perferendis eius voluptatem repellat.
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel fuga consequatur voluptatum ratione. Cumque libero voluptas esse, at animi repellendus, quibusdam facere veniam dolor tempore, fugiat perferendis eius voluptatem repellat.</p>
+
     <button onClick={()=>getoutbtn()} id="btn">GET-OUT</button>
+    </div>
     </>
   );
 };
--- AmrhitPradhan-Day1-Task/Login.jsx
+++ AmrhitPradhan-Day1-Task/Login.jsx
@@ -1,60 +1,63 @@
 import React, { useState, useEffect } from "react";
 
-const Login = ({login}) => {
+const Login = ({ login }) => {
   const [userid, setuserid] = useState("");
   const [passcode, setpasscode] = useState("");
   const [localstorage, getlocalstorage] = useState(false);
   // const [msg, setMsg] = useState("");
 
-// const localStorecheck = localStorage.getItem("entrypassed");
+  // const localStorecheck = localStorage.getItem("entrypassed");
 
 
- 
+
   useEffect(() => {
-    if (localStorage.getItem("entrypassed")) { 
+    if (localStorage.getItem("entrypassed")) {
       login();
-    } 
-  },[localstorage]);
-  
+    }
+  }, [localstorage]);
+
   const checkpoint = () => {
     let defaultuser = "admin",
-  defaultpass = "adminzz";
+      defaultpass = "adminzz";
     if (userid !== "" && passcode !== "") {
       if (userid === defaultuser && passcode === defaultpass) {
         getlocalstorage(true);
         login();
       } else {
-        alert("Wrong passcode");
+        alert("WRONG PASSCODE");
       }
     } else {
-      alert('cannt be empty')
+      alert('USER-ID and PASS-CODE cannot be empty!!!!!');
     }
   }
 
   return (
     <>
-      <div>
-        <div  className="login-main">
-          <div className="login-heading">
-            <span>ENTRY-POINT</span>
-          </div>
+      <div className="login-main">
 
-          <div className="login-user">
-            <label htmlFor="userid" className="login-labelname">USER-ID</label> <br />
-            <input type="text" id="userid" placeholder="eg: Jung-Bdr" onChange={(e) => setuserid(e.target.value)} value={userid} className="login-input" />
-          </div>
-
-          <div className="login-pass">
-            <label htmlFor="passcode" className="login-labelname">PASS-CODE</label> <br />
-            <input type="password" id="passcode" placeholder="secret code" onChange={(e) => setpasscode(e.target.value)} value={passcode} className="login-input"/>
-          </div>
-      
-
-          <button onClick={() => checkpoint()} id="btn"> GET-IN </button>
-     
+        <div className="login-heading">
+          <span>ENTRY-POINT</span>
         </div>
 
+       
+        <label htmlFor="userid" className="login-labelname">USER-ID</label> <br />
+        <div className="inputclass">
+          <input type="text" id="userid" placeholder="eg: Jung-Bdr" onChange={(e) => setuserid(e.target.value)} value={userid} className="login-input" />
+        </div>
+    
+        <label htmlFor="passcode" className="login-labelname">PASS-CODE</label> <br />
+        <div className="inputclass">
+          <input type="password" id="passcode" placeholder="secret code" onChange={(e) => setpasscode(e.target.value)} value={passcode} className="login-input" />
+        </div>
+     
+      <div className="btnclass">
+
+        <button onClick={() => checkpoint()} id="btn"> GET-IN </button>
       </div>
+
+    </div>
+
+        
     </>
   );
 }
